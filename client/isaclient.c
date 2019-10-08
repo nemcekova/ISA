e9751ab95294a163f1bfce06c7d76b0af3a78871
nemcekova <nemcek.barbora@gmail.com>
2019-10-08 15:39:45 +0200

    arg

---
+++ client/isaclient.c
@@ -0,0 +1,14 @@
+/* Project: Programovanie sietovej sluzby
+*  variant: HTTP nastenka
+*  author: Barbora Nemcekova
+*  login: xnemce06
+*  date: 08.10.2019
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]){
+}
