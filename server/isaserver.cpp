454583f9d87b4eb710b83eae774ec8188b4782ab
nemcekova <nemcek.barbora@gmail.com>
2019-10-15 23:28:30 +0200

    zoznam nasteniek a obsah nasteniek almost done

--- server/isaserver.cpp
+++ server/isaserver.cpp
@@ -14,38 +14,131 @@
 
 
 class Board{
+    std::string name;
     public:
-        Board(){
-            std::cout << "Hello World!";
-        }
-        ~Board();
-        std::string name;
         
-        //vyhladavanie ci nastenka s danym nazvom existuje
-        bool BoardExists(std::string name){
-            for (unsigned int i = 0; i < nastenky.size(); i++){
-                if(name.compare(nastenky[0]) == 0){
-                    return true;
-                }
-                else continue;
+        Board(std::string boardname){
+            std::cout << "Hello World!\n";
+            name = boardname;
+            //AddNewBoard(name);
+            //nastenky.push_back(name);
+        }
+        ~Board(){
+            std::cout << "Bye World!\n";
+        }
+        //std::string name;
+        
+        std::string GetName(){
+            return(name);
+        }
+        
+        void AddContent(std::string text){
+            if(content.empty() == true){
+                i = 1;
+                content.insert(std::pair<int, std::string>(i, text));
+                i++;
             }
-            return false;
+            else{
+                content.insert(std::pair<int, std::string>(i, text));
+                i++;
+            }
         }
         
-        void AddNewBoard(std::string name){
-            nastenky.push_back(name);
-            printf("%s\n", name);
+        void DeleteContent(int id){
+            bool changing = false;
+            std::string key;
+            std::map<int,std::string>::iterator it = content.begin();
+            for (it=content.begin(); it!=content.end(); ++it){
+                /*if(changing == true){
+                    it->first = key; ///////////NEFUNGUJE TOTO JE READ ONLY
+                    key++;
+                }*/
+                if(it->first == id){
+                std::map<int, std::string>::iterator dupe = it;
+                //std::map<int, std::string>::iterator dupe2 = dupe2;
+                for(dupe = it; dupe != content.end();++dupe){
+                    //for(dupe2 = dupe; dupe2!=content.end();++dupe2){
+                        key = dupe->second;
+                        it->second = key;
+                    //}
+                    //content.erase(it);
+                    }
+                }
+            }
+        }
+        
+        std::map<int, std::string> GetContent(){
+            return(content);
         }
         
     private:
-        std::vector<std::string> nastenky; // zoznam aktualnych nasteniek
-        std::map<std::string, int> content; //mapovanie obsahu na nastenku
+        std::map<int, std::string> content; //mapovanie obsahu na nastenku
+        int i;
+        //void AddNewBoard(std::string name){
+            
+        //}
+};
+
+/////////////////////////////////////////////////////////////////////////////////////
+class List{
+    std::string name;
+    std::vector<std::string> nastenky; // zoznam aktualnych nasteniek
+public:
+    List(std::string listname){
+        name = listname;
+    }
+    
+    ~List(){
+        name = "";
+    }
+
+    /*
+    *Checks if board with specific name already exists
+    */
+    bool BoardExists(std::string BoardName){
+        for (unsigned int i = 0; i < nastenky.size(); i++){
+            if(BoardName.compare(nastenky[i]) == 0){
+                std::cout << "true\n";
+                return true;
+            }
+            else continue;
+        }
+        std::cout << "false\n";
+        return false;
+    }
+
+    /*  
+    * add new board to the list 
+    */
+    void AddNewBoard(std::string nazov){
+        bool exists = BoardExists(nazov);
+        if(exists == false){
+            nastenky.push_back(nazov);
+        }
+    }
+    
+    /*
+    *delete board from the list of boards
+    */
+    void DeleteBoard(std::string nazov){
+        std::vector<std::string>::iterator it;
         
-        
+        it = find(nastenky.begin(), nastenky.end(), nazov);
+        if(it != nastenky.end()){
+            nastenky.erase(it);
+        }
+    }
+    
+    /*
+    *returns list of boards
+    */
+    std::vector<std::string> GetNastenky(){
+        return(nastenky);
+    }
     
 };
 
-
+////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[]){
     char *portnum;
     if(argc > 3){
@@ -75,13 +168,50 @@ int main(int argc, char *argv[]){
     
     
     //undefined reference to `Board::~Board()'
-    Board nova;
-    nova.AddNewBoard("prva");
+    Board nova("prva");
+    List zoznam("novy");
+    zoznam.AddNewBoard(nova.GetName());
+    Board nastenka("druha");
+    zoznam.AddNewBoard(nastenka.GetName());
+    
+    Board bbb("tretia");
+    zoznam.AddNewBoard(bbb.GetName());
+    
+    std::vector<std::string> nastenky = zoznam.GetNastenky();
+    for(unsigned int i =0; i<nastenky.size(); i++){
+        std::cout << nastenky[i] + "\n";
+        
+    }
+    
+    zoznam.DeleteBoard(nova.GetName());
+    std::vector<std::string> naste = zoznam.GetNastenky();
+    for(unsigned int o =0; o<naste.size(); o++){
+        std::cout << naste[o] + "\n";
+        
+    }
+    
+    nova.AddContent("aaaaaaaaa\n");
+    nova.AddContent("bbbbbbbbb\n");
+    nova.AddContent("ccccccccc\n");
+    nova.AddContent("ddddddddd\n");
+    nova.AddContent("eeeeeeeee\n");
+    nova.AddContent("fffffffff\n");
+    nova.AddContent("ggggggggg\n");
+    
+    std::map<int, std::string> obsah = nova.GetContent();
     
     
+    std::map<int,std::string>::iterator it = obsah.begin();
+    for (it=obsah.begin(); it!=obsah.end(); ++it)
+    std::cout << it->first << " " << it->second << '\n';
+    //nova.AddNewBoard("prva");
     
+    nova.DeleteContent(2);
     
-    
+    std::map<int, std::string> obsa = nova.GetContent();
+     it = obsa.begin();
+    for (it=obsa.begin(); it!=obsa.end(); ++it)
+    std::cout << it->first << " " << it->second << '\n';
     
     
     
--- server/isaserver.h
+++ server/isaserver.h
@@ -13,4 +13,6 @@
 #include<map>
 #include<string>
 #include<iostream>
+#include<algorithm>
+#include<iterator>
 
